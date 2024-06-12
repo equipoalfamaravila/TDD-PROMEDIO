bb6553ff1d4e5893d961e31fe74b1c4e421c7b8e
Argan <71340325@continental.edu.pe>
2024-06-12 01:00:22 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoPromedio/calcularPromedio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPromedio
+{
+    public class calcularPromedio
+    {
+        public static double calPromedio(int[] conjunto)
+        { 
+            int suma = 0; 
+            foreach (int numero in conjunto)
+            {
+                suma += numero;
+            }
+
+            return (double)suma / conjunto.Length;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProyectoPromedio;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestConjuntoConNumerosPositivos()
+        {
+            int[] conjunto =  { 1, 2, 3, 4, 5 };
+            double actual = calcularPromedio.calPromedio(conjunto); 
+            double esperado = 3;
+            Assert.AreEqual(esperado, actual);
+        }
+
+        [TestMethod]
+        public void TestConjuntoConNumerosNegativos()
+        {
+            int[] conjunto = { -1, -2, -3, -4, -5 };
+            double actual = calcularPromedio.calPromedio(conjunto);
+            double esperado = -3;
+            Assert.AreEqual(esperado, actual);
+        }
+
+        [TestMethod]
+        public void TestConjuntoMixto()
+        {
+            int[] conjunto = { -1, 2, -3, 4, -5 };
+            double actual = calcularPromedio.calPromedio(conjunto);
+            double esperado = -6;
+            Assert.AreEqual(esperado, actual);
+        }
+
+        [TestMethod]
+        public void TestConjuntoConUnSoloElemento()
+        {
+            int[] conjunto = { 42 };
+            double actual = calcularPromedio.calPromedio(conjunto);
+            double esperado = 42;
+            Assert.AreEqual(esperado, actual);
+        }
+
+        [TestMethod]
+        public void TestConjuntoConValoresRepetidos()
+        {
+            int[] conjunto = new int[] { 5, 5, 5, 5, 5 };
+            double actual = calcularPromedio.calPromedio(conjunto);
+            double esperado = 5;
+            Assert.AreEqual(esperado, actual);
+        }
+    }
+}
